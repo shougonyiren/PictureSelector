7739e13ae375d21ba9d7c5a4ee0c1cfa34dd559a
liuhao56 <liuhao56@maoyan.com>
2020-04-13 16:20:06 +0800

    获取相册列表

--- app/src/main/java/com/luck/pictureselector/ui/setting/SettingsActivity.java
+++ app/src/main/java/com/luck/pictureselector/ui/setting/SettingsActivity.java
@@ -1,5 +1,6 @@
 package com.luck.pictureselector.ui.setting;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Build;
@@ -7,29 +8,46 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 
+import com.luck.picture.lib.adapter.PictureAlbumDirectoryAdapter;
+import com.luck.picture.lib.config.PictureConfig;
 import com.luck.picture.lib.config.PictureSelectionConfig;
 import com.luck.picture.lib.dialog.PictureLoadingDialog;
 import com.luck.picture.lib.entity.LocalMedia;
 import com.luck.picture.lib.entity.LocalMediaFolder;
 import com.luck.picture.lib.model.LocalMediaLoader;
+import com.luck.picture.lib.permissions.PermissionChecker;
+import com.luck.picture.lib.widget.FolderPopWindow;
 import com.luck.pictureselector.R;
 import com.luck.pictureselector.service.ImageAnalysisIntentService;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class SettingsActivity extends AppCompatActivity {
-
+public class SettingsActivity extends AppCompatActivity implements View.OnClickListener,
+        PictureAlbumDirectoryAdapter.OnItemClickListener {
+    //todo 目录图片
     private PictureSelectionConfig config;
     private static final int SHOW_DIALOG = 0;
     private static final int DISMISS_DIALOG = 1;
     private PictureLoadingDialog mLoadingDialog;
+
+    protected List<LocalMedia> images = new ArrayList<>();
+    protected List<LocalMediaFolder> foldersList = new ArrayList<>();
+
+    private FolderPopWindow folderWindow;
+    private ImageView mIvArrow;
+    private RelativeLayout image_analysis_bg;
+    private TextView test;
+
 
     private  TextView textView;
     private  LocalMediaLoader mediaLoader;
@@ -65,14 +83,56 @@ public class SettingsActivity extends AppCompatActivity {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
         Intent intent=new Intent(this,ImageAnalysisIntentService.class);
+        test=findViewById(R.id.test1111);
+        test.setOnClickListener(this);
+        mIvArrow=findViewById(R.id.ivArrow);
+        image_analysis_bg=findViewById(R.id.image_analysis_bg);
         textView=findViewById(R.id.tv_image_analysis);
         textView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                loadAllMediaData();
                 //Todo 执行service
-                startService(intent);
+                //startService(intent);
             }
         });
+        folderWindow = new FolderPopWindow(this, config);
+        folderWindow.setArrowImageView(mIvArrow);
+        folderWindow.setOnItemClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        if (id==R.id.test1111){
+            folderWindow.showAsDropDown(image_analysis_bg);
+        }
+/*        if (id == com.luck.picture.lib.R.id.picture_title || id == com.luck.picture.lib.R.id.ivArrow) {
+            if (folderWindow.isShowing()) {
+                folderWindow.dismiss();
+            } else {
+                if (images != null && images.size() > 0) {
+                    folderWindow.showAsDropDown(image_analysis_bg);
+                }
+            }
+        }*/
+    }
+
+    /**
+     * 加载数据
+     */
+    private void loadAllMediaData() {
+        if (PermissionChecker
+                .checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) &&
+                PermissionChecker
+                        .checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+            readLocalMedia();
+        } else {
+            PermissionChecker.requestPermissions(this, new String[]{
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE}, PictureConfig.APPLY_STORAGE_PERMISSIONS_CODE);
+        }
     }
 
     /**
@@ -84,7 +144,7 @@ public class SettingsActivity extends AppCompatActivity {
             mediaLoader = new LocalMediaLoader(this, config);
         }
         mediaLoader.loadAllMedia();
-/*        mediaLoader.setCompleteListener(new LocalMediaLoader.LocalMediaLoadListener() {
+        mediaLoader.setCompleteListener(new LocalMediaLoader.LocalMediaLoadListener() {
             @Override
             public void loadComplete(List<LocalMediaFolder> folders) {
                 if (folders.size() > 0) {
@@ -95,40 +155,9 @@ public class SettingsActivity extends AppCompatActivity {
                     if (images == null) {
                         images = new ArrayList<>();
                     }
-                    // 这里解决有些机型会出现拍照完，相册列表不及时刷新问题
-                    // 因为onActivityResult里手动添加拍照后的照片，
-                    // 如果查询出来的图片大于或等于当前adapter集合的图片则取更新后的，否则就取本地的
-                    int currentSize = images.size();
-                    int resultSize = result.size();
-                    oldCurrentListSize = oldCurrentListSize + currentSize;
-                    if (resultSize >= currentSize) {
-                        if (currentSize > 0 && currentSize < resultSize && oldCurrentListSize != resultSize) {
-                            // 这种情况多数是由于拍照导致Activity和数据被回收数据不一致
-                            images.addAll(result);
-                            // 更新相机胶卷目录
-                            LocalMedia media = images.get(0);
-                            folder.setFirstImagePath(media.getPath());
-                            folder.getImages().add(0, media);
-                            folder.setCheckedNum(1);
-                            folder.setImageNum(folder.getImageNum() + 1);
-                            // 更新相片所属目录
-                            updateMediaFolder(foldersList, media);
-                        } else {
-                            // 正常情况下
-                            images = result;
-                        }
-                        folderWindow.bindFolder(folders);
-                    }
-                }
-                if (mAdapter != null) {
-                    mAdapter.bindImagesData(images);
-                    boolean isEmpty = images.size() > 0;
-                    if (!isEmpty) {
-                        mTvEmpty.setText(getString(com.luck.picture.lib.R.string.picture_empty));
-                        mTvEmpty.setCompoundDrawablesRelativeWithIntrinsicBounds
-                                (0, com.luck.picture.lib.R.drawable.picture_icon_no_data, 0, 0);
-                    }
-                    mTvEmpty.setVisibility(isEmpty ? View.INVISIBLE : View.VISIBLE);
+                    // 正常情况下    当有些机型会出现拍照完，相册列表不及时刷新问题  解决-》 PictureSelectorActivity
+                    images = result;
+                    folderWindow.bindFolder(folders);
                 }
                 mHandler.sendEmptyMessage(DISMISS_DIALOG);
             }
@@ -136,14 +165,10 @@ public class SettingsActivity extends AppCompatActivity {
             @Override
             public void loadMediaDataError() {
                 mHandler.sendEmptyMessage(DISMISS_DIALOG);
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
-                    mTvEmpty.setCompoundDrawablesRelativeWithIntrinsicBounds
-                            (0, com.luck.picture.lib.R.drawable.picture_icon_data_error, 0, 0);
-                }
-                mTvEmpty.setText(getString(com.luck.picture.lib.R.string.picture_data_exception));
-                mTvEmpty.setVisibility(images.size() > 0 ? View.INVISIBLE : View.VISIBLE);
+                Toast.makeText(getBaseContext(), getString(com.luck.picture.lib.R.string.picture_data_exception),
+                        Toast.LENGTH_SHORT).show();
             }
-        });*/
+        });
 
     }
 
@@ -187,4 +212,8 @@ public class SettingsActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    public void onItemClick(boolean isCameraFolder, String folderName, List<LocalMedia> images) {
+        //todo 点击
+    }
 }
